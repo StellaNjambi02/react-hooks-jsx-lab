51ac0a6bae1a0b6ed9e482a0107ae5e49fa2410c
StellaNjambi02 <njambindungistella@gmail.com>
2024-08-01 12:50:57 +0300

    JSX lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+const attribute={
+  color: 'firebrick',
+}
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return <div id="home">
+    <h1 style={attribute}>{name} is a Web Developer from {city}</h1>
+  </div>;
 }
 
 export default Home;
